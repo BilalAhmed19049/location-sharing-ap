27d908b2eb9e6a0f103baa14a05367f1f9892cd2
Bilal Ahmed <bilal.ahmed@techswivel.com>
2023-10-27 10:25:38 +0500

    commit to working branch

--- lib/Models/firebase_model.dart
+++ lib/Models/firebase_model.dart
@@ -1,6 +1,6 @@
 class FirebaseModel {
   late String firstname, lastname, email, password, id;
-
+//constructor
   FirebaseModel({
     required this.firstname,
     required this.lastname,
@@ -8,6 +8,7 @@ class FirebaseModel {
     required this.password,
   });
 
+//named constructor
   FirebaseModel.fromMap(Map<String, dynamic> locationData) {
     firstname = locationData['firstname'];
     lastname = locationData['lastname'];
@@ -16,6 +17,7 @@ class FirebaseModel {
     id = locationData['id'];
   }
 
+//named constructor
   Map<String, dynamic> toMap() {
     return {
       'firstname': firstname,
