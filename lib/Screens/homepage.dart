019c7c56c21acb50028d60189b3b89f987cd5516
Bilal Ahmed <bilal.ahmed@techswivel.com>
2023-11-01 15:19:49 +0500

    commit to working branch

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,3 +1,4 @@
+// Importing necessary packages
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -7,6 +8,7 @@ import 'package:permission_handler/permission_handler.dart';
 import 'all_users_location.dart';
 import 'my_map_screen.dart';
 
+// Creating a StatefulWidget named Homepage
 class Homepage extends StatefulWidget {
   Homepage({super.key});
 
@@ -14,7 +16,9 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => _HomepageState();
 }
 
+// Creating the state for Homepage
 class _HomepageState extends State<Homepage> {
+  // Getting the current user
   final user = FirebaseAuth.instance.currentUser!;
   final loc.Location location = loc.Location();
   StreamSubscription<loc.LocationData>? _locationSubscription;
@@ -35,68 +39,79 @@ class _HomepageState extends State<Homepage> {
         child: Column(
           children: [
             Text(user.email!),
+            // Displaying user email
             Text(user.uid!),
+            // Displaying user UID
             TextButton(
                 onPressed: () {
                   _getLocation(user.uid);
-                  print('get location working');
+                  print(
+                      'get location working'); // Triggering location retrieval
                 },
                 child: Text('Add Location')),
+            // Button to add location
             TextButton(
                 onPressed: () {
                   _listenLocation(user.uid);
                 },
                 child: Text('Enable Live Location')),
+            // Button to enable live location
             TextButton(
                 onPressed: () {
                   _stopListening();
                 },
                 child: Text('Disable live location')),
+            // Button to disable live location
             Expanded(
                 child: StreamBuilder(
-              stream:
+                  stream:
                   FirebaseFirestore.instance.collection('location').snapshots(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                return ListView.builder(
-                    itemCount: snapshot.data?.docs.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title:
+                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    return ListView.builder(
+                        itemCount: snapshot.data?.docs.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title:
                             Text(snapshot.data!.docs[index]['name'].toString()),
-                        subtitle: Row(
-                          children: [
-                            Text(snapshot.data!.docs[index]['latitude']
-                                .toString()),
-                            SizedBox(
-                              width: 20,
+                            // Displaying name
+                            subtitle: Row(
+                              children: [
+                                Text(snapshot.data!.docs[index]['latitude']
+                                    .toString()), // Displaying latitude
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Text(snapshot.data!.docs[index]['longitude']
+                                    .toString()), // Displaying longitude
+                              ],
                             ),
-                            Text(snapshot.data!.docs[index]['longitude']
-                                .toString()),
-                          ],
-                        ),
-                        trailing: IconButton(
-                          icon: Icon(Icons.directions),
-                          onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) =>
-                                    MyMap(snapshot.data!.docs[index].id)));
-                          },
-                        ),
-                      );
-                    });
-              },
-            )),
+                            trailing: IconButton(
+                              icon: Icon(Icons.directions),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) =>
+                                        MyMap(snapshot.data!.docs[index]
+                                            .id))); // Navigating to map screen
+                              },
+                            ),
+                          );
+                        });
+                  },
+                )),
             TextButton(
                 onPressed: () {
                   Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => AllUsers()));
+                      MaterialPageRoute(builder: (context) =>
+                          AllUsers())); // Navigating to all users location screen
                 },
                 child: Text('Show live location')),
+            // Button to show live locations
             ElevatedButton(
               onPressed: () => FirebaseAuth.instance.signOut(),
+              // Signing out user
               child: Text('Sign out'),
             )
           ],
@@ -105,6 +120,7 @@ class _HomepageState extends State<Homepage> {
     );
   }
 
+  // Function to get current location and add it to Firestore
   _getLocation(String userId) async {
     try {
       final loc.LocationData _locationResult = await location.getLocation();
@@ -112,12 +128,13 @@ class _HomepageState extends State<Homepage> {
         'latitude': _locationResult.latitude,
         'longitude': _locationResult.longitude,
         'name': user.displayName,
-      }, SetOptions(merge: true));
+      }, SetOptions(merge: true)); // Setting location data in Firestore
     } catch (error) {
       print(error);
     }
   }
 
+  // Function to start listening for live location updates
   Future<void> _listenLocation(String userId) async {
     _locationSubscription = location.onLocationChanged.handleError((onError) {
       print(onError);
@@ -130,10 +147,11 @@ class _HomepageState extends State<Homepage> {
         'latitude': currentlocation.latitude,
         'longitude': currentlocation.longitude,
         'name': user.displayName,
-      }, SetOptions(merge: true));
+      }, SetOptions(merge: true)); // Updating location data in Firestore
     });
   }
 
+  // Function to stop listening for live location updates
   _stopListening() {
     _locationSubscription?.cancel();
     setState(() {
@@ -141,6 +159,7 @@ class _HomepageState extends State<Homepage> {
     });
   }
 
+  // Function to request location permission
   _requestPermission() async {
     var status = await Permission.location.request();
     if (status.isGranted) {
@@ -148,7 +167,7 @@ class _HomepageState extends State<Homepage> {
     } else if (status.isDenied) {
       _requestPermission();
     } else if (status.isPermanentlyDenied) {
-      openAppSettings();
+      openAppSettings(); // Open app settings to allow location access
     }
   }
 }
